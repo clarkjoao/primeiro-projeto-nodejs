ed0c5e71237f61a9be7257c27017b016de924a44
clark <clark.joao@gmail.com>
2020-04-15 00:19:57 -0300

    Fix/GetTransactionsRoute

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -17,6 +17,13 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    const getBalance = this.transactionsRepository.getBalance().total;
+    if (type === 'outcome' && getBalance < value) {
+      throw Error(
+        'The value of the outcome is greater than tha total in your account.',
+      );
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
